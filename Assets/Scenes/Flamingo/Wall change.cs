e1145f73bdd60a78356af802df0acaaa419e60b3
AggelosTsil Official <jesusisatigerofficial@gmail.com>
2025-12-03 15:26:54 +0200

    Flaming game now changes legs after set timer

--- Assets/Scenes/Flamingo/Wall change.cs
+++ Assets/Scenes/Flamingo/Wall change.cs
@@ -1,23 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Wallchange : MonoBehaviour
 {
     public PoseDetectorScript pose;
+
+    public PoseModelHelper RightLeg;
+    public PoseModelHelper LeftLeg;
     public Material Footdown;
     public Material Footup;
+
+    public Text ChangeText;
+
+    public float Timer;
+    public float ChangeTextLifespan;
+    float TimerIni;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimerIni = Timer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Timer >= 0)
+        {
+            Timer -= Time.deltaTime;
+        }
+        else if (Timer == Timer - ChangeTextLifespan)
+        {
+            ChangeText.text = "";
+        }
+        else
+        {
+            Timer = TimerIni;
+            if (pose.poseModel == RightLeg)
+            {
+                pose.poseModel = LeftLeg;
+            }
+            else
+            {
+                pose.poseModel = RightLeg;
+            }
+            ChangeText.text = "Change";
+        }
     }
 
     void LateUpdate()
