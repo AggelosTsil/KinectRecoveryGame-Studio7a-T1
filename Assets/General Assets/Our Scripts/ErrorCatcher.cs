4c0e60ebdb291c7e45742e059d00b79f0d94dc6c
AggelosTsil Official <jesusisatigerofficial@gmail.com>
2026-02-04 16:54:14 +0200

    ErrorCatcher now prints out screenshots in a folder next to the exe. If that file doesnt exist it makes one.

--- Assets/General Assets/Our Scripts/ErrorCatcher.cs
+++ Assets/General Assets/Our Scripts/ErrorCatcher.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ErrorCatcher : MonoBehaviour
 {
+    private string directory;
     // Start is called before the first frame update
     void Start()
     {
+        directory = Path.Combine(Directory.GetParent(Application.dataPath).FullName, "Screenshots"); //sets screenshot path to screenshots folder
         
+        if (!Directory.Exists(directory)) //If no screenshots folder exists
+        {
+            Directory.CreateDirectory(directory); //creates one
+            Debug.Log(directory + " directory created");
+        }
     }
 
     // Update is called once per frame
@@ -20,7 +28,8 @@ public class ErrorCatcher : MonoBehaviour
     public void PrintError()
     {
         string filename = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
-        ScreenCapture.CaptureScreenshot("/Assets/Screenshots/" + filename);
+        ScreenCapture.CaptureScreenshot(Path.Combine(directory, filename));
+        Debug.Log("Took screenshot " + Path.Combine(directory, filename));
     }
 
     public void Screenshot(InputAction.CallbackContext context)
