a7cb800b682e203fc4bc97d513607646f2572107
AggelosTsil Official <jesusisatigerofficial@gmail.com>
2026-02-04 16:23:12 +0200

    Readying up for controlls test

--- Assets/General Assets/Our Scripts/ErrorCatcher.cs
+++ Assets/General Assets/Our Scripts/ErrorCatcher.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ErrorCatcher : MonoBehaviour
 {
@@ -14,5 +15,16 @@ public class ErrorCatcher : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PrintError()
+    {
+        string filename = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+        ScreenCapture.CaptureScreenshot("/Assets/Screenshots/" + filename);
+    }
+
+    public void Screenshot(InputAction.CallbackContext context)
+    {
+        PrintError();
     }
 }
