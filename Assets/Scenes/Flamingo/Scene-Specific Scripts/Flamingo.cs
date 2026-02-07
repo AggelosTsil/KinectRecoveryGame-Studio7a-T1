24dbd3c821b07fd2a7ea9d0bf4dd9bb1a6ef7a7c
AggelosTsil Official <jesusisatigerofficial@gmail.com>
2026-02-07 20:11:17 +0200

    Flamingos fly out and dissapear after getting punched MOST flamingos move in after their neighbour was punched but that doesnt really work well with the skliro karioli

--- Assets/Scenes/Flamingo/Scene-Specific Scripts/Flamingo.cs
+++ Assets/Scenes/Flamingo/Scene-Specific Scripts/Flamingo.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Flamingo : MonoBehaviour
@@ -32,15 +33,61 @@ public class Flamingo : MonoBehaviour
                 if ((child.transform.localPosition.x > -1) && (child.transform.localPosition.x < 0)) //this is bound to the position of the flamingos in world space
                 {
                     Debug.Log("punched LEFT");
-                    child.GetComponent<Animator>().SetTrigger("Death");
+                    Animator anim = child.GetComponent<Animator>();
+                    Vector3 origin = child.transform.localPosition;
+                    anim.SetTrigger("Left");
+                    anim.SetTrigger("Death");
+
+                    //brings in next flamingo
+                    Move(direction, origin);
+
+                    //removes punched flamingo
+                    Destroy(child.gameObject, 3f);
                 }
             }
             else if (direction == "RIGHT")
             {
                 if ((child.transform.localPosition.x < 3) && (child.transform.localPosition.x > 0))
                 {
-                    Debug.Log("punched RIGH");
+                    Debug.Log("punched RIGHT");
+                    Vector3 origin = child.transform.localPosition;
                     child.GetComponent<Animator>().SetTrigger("Death");
+
+                    //brings in next flamingo
+                    Move(direction, origin);
+
+                    //removes punched flamingo
+                    Destroy(child.gameObject, 3f);
+
+                    
+                }
+            }
+        }
+    }
+
+    void Move(string direction, Vector3 pos)
+    {
+        if (direction == "RIGHT")
+        {
+            foreach (Transform child in this.transform)
+            {
+                if (child.transform.localPosition.x > 3)
+                {
+                    Debug.Log("moving  " + child.gameObject + " to position " + pos);
+                    //child.localPosition = Vector3.MoveTowards(child.localPosition, pos, Time.deltaTime * 0.1f);
+                    child.localPosition = pos;
+                }
+            }
+        }
+        else
+        {
+            foreach (Transform child in this.transform)
+            {
+                if (child.transform.localPosition.x < -1)
+                {
+                    Debug.Log("moving  " + child.gameObject + " to position " + pos);
+                    //child.localPosition = Vector3.MoveTowards(child.localPosition, pos, Time.deltaTime * 0.1f);
+                    child.localPosition = pos;
                 }
             }
         }
