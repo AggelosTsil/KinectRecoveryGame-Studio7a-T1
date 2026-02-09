67ffa21a6ec573dddae04cffeb04a38312742b12
georgethepallis <georgepallis03@gmail.com>
2026-02-09 13:59:20 +0200

    can now drag and drop gamecards to rearrange them

---
+++ Assets/Scenes/Main Menu/PlaylistDropZone.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlaylistDropZone : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("DROP CALLED");
+
+        GameCardDrag drag =
+            eventData.pointerDrag.GetComponent<GameCardDrag>();
+
+        if (drag == null) return;
+
+        RectTransform draggedRect =
+            drag.GetComponent<RectTransform>();
+
+        int newIndex = GetIndexFromPosition(draggedRect.position);
+
+        drag.transform.SetParent(transform);
+        drag.transform.SetSiblingIndex(newIndex);
+
+        UpdatePlaylistOrder();
+    }
+
+    int GetIndexFromPosition(Vector2 dropPosition)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            RectTransform child =
+                transform.GetChild(i).GetComponent<RectTransform>();
+
+            if (dropPosition.y > child.position.y)
+                return i;
+        }
+
+        return transform.childCount;
+    }
+
+    void UpdatePlaylistOrder()
+    {
+        Patient patient =
+            DataManager.instance.CurrentPatient;
+
+        if (patient == null) return;
+
+        patient.GamePlaylist.Clear();
+
+        foreach (Transform child in transform)
+        {
+            GameCard card = child.GetComponent<GameCard>();
+            if (card != null)
+                patient.GamePlaylist.Add(card.sessionData);
+        }
+
+        Debug.Log("Playlist order updated");
+    }
+}
